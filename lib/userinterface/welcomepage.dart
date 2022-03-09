7746e243c3f03fde8b653efa3998c6732d4b957e
ashrafiabir01 <ashrafiabir04@gmail.com>
2022-03-09 23:20:18 +0600

    nias2

--- lib/userinterface/welcomepage.dart
+++ lib/userinterface/welcomepage.dart
@@ -604,8 +604,211 @@ class _WelcomePageState extends State<WelcomePage> {
           ),
         ),
       ],
+    ), //
+//Page 3
+    Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Container(
+          width: 1000,
+          child: Row(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("   1",
+                          style: TextStyle(
+                              color: Color(0xFF808080), fontSize: 20)),
+                      Text("  {",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  ),
+                  Text("2",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("3",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("4",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("5",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("6",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("7",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("8",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Text("9",
+                      style: TextStyle(color: Color(0xFF808080), fontSize: 20)),
+                  SizedBox(height: 4),
+                  Row(
+                    children: [
+                      Text("   10",
+                          style: TextStyle(
+                              color: Color(0xFF808080), fontSize: 20)),
+                      Text("  }",
+                          style: TextStyle(color: Colors.white, fontSize: 20)),
+                    ],
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Column(
+                  children: [
+                    SizedBox(height: 50),
+
+                    // Row(
+                    //   children: [
+                    //     Text('''"facebook"''',
+                    //         style: TextStyle(
+                    //             color: Color(0xFFD7F484), fontSize: 20)),
+                    //     Text(''' : ''',
+                    //         style:
+                    //             TextStyle(color: Colors.white, fontSize: 20)),
+                    //     Text('''"ashrfiabir"''',
+                    //         style: TextStyle(
+                    //             color: Color(0xFFF9826C), fontSize: 20)),
+                    //     Text(''' ,''',
+                    //         style:
+                    //             TextStyle(color: Colors.white, fontSize: 20)),
+                    //   ],
+                    // ),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"name"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"Ashrafi Abir"                          ',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"email"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"ashrafiabir04@gmail.com"',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"github"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"ashrafiabir01"                     ',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"twitter"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"ashrafi_abir"                       ',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"facebook"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"ashrafiabir04"               ',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 2.5),
+                    RichText(
+                      text: TextSpan(
+                        children: <TextSpan>[
+                          TextSpan(
+                              text: '''"website"''',
+                              style: TextStyle(
+                                  color: Color(0xFFD7F484), fontSize: 20)),
+                          TextSpan(
+                              text: ' : ',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20)),
+                          TextSpan(
+                            text: '"ashrafiabir.me"                ',
+                            style: TextStyle(
+                                color: Color(0xFFF9826C), fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     ),
-    Container(),
     Container()
   ];
   @override
@@ -818,60 +1021,81 @@ class _WelcomePageState extends State<WelcomePage> {
                             SizedBox(
                               height: 10,
                             ),
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: Row(
-                                  children: [
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Image.asset(
-                                      "assets/html.png",
-                                      height: 18,
-                                      width: 18,
-                                    ),
-                                    Text("  index.html",
-                                        style: TextStyle(color: Colors.white))
-                                  ],
-                                )),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  PagesIndex = 0;
+                                });
+                              },
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Image.asset(
+                                        "assets/html.png",
+                                        height: 18,
+                                        width: 18,
+                                      ),
+                                      Text("  index.html",
+                                          style: TextStyle(color: Colors.white))
+                                    ],
+                                  )),
+                            ),
                             SizedBox(
                               height: 10,
                             ),
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: Row(
-                                  children: [
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Image.asset(
-                                      "assets/dart.png",
-                                      height: 18,
-                                      width: 18,
-                                    ),
-                                    Text("  project.dart",
-                                        style: TextStyle(color: Colors.white))
-                                  ],
-                                )),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  PagesIndex = 1;
+                                });
+                              },
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Image.asset(
+                                        "assets/dart.png",
+                                        height: 18,
+                                        width: 18,
+                                      ),
+                                      Text("  project.dart",
+                                          style: TextStyle(color: Colors.white))
+                                    ],
+                                  )),
+                            ),
                             SizedBox(
                               height: 10,
                             ),
-                            Align(
-                                alignment: Alignment.topLeft,
-                                child: Row(
-                                  children: [
-                                    SizedBox(
-                                      width: 10,
-                                    ),
-                                    Image.asset(
-                                      "assets/json.png",
-                                      height: 18,
-                                      width: 18,
-                                    ),
-                                    Text("  contact.json",
-                                        style: TextStyle(color: Colors.white))
-                                  ],
-                                )),
+                            InkWell(
+                              onTap: () {
+                                setState(() {
+                                  PagesIndex = 2;
+                                });
+                              },
+                              child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Row(
+                                    children: [
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Image.asset(
+                                        "assets/json.png",
+                                        height: 18,
+                                        width: 18,
+                                      ),
+                                      Text("  contact.json",
+                                          style: TextStyle(color: Colors.white))
+                                    ],
+                                  )),
+                            ),
                             SizedBox(
                               height: 10,
                             ),
@@ -916,7 +1140,7 @@ class _WelcomePageState extends State<WelcomePage> {
             ),
           ),
           Container(
-            width: MediaQuery.of(context).size.width - 235,
+            width: MediaQuery.of(context).size.width - 232,
             child: Column(children: [
               SizedBox(height: 2),
               Container(
@@ -924,6 +1148,10 @@ class _WelcomePageState extends State<WelcomePage> {
                 height: 50,
                 child: Row(
                   children: [
+                    SizedBox(
+                      width: 1,
+                      child: Container(color: Colors.black),
+                    ),
                     InkWell(
                       onTap: () {
                         setState(() {
@@ -976,21 +1204,28 @@ class _WelcomePageState extends State<WelcomePage> {
                       width: 1,
                       child: Container(color: Colors.black),
                     ),
-                    Container(
-                        child: Row(
-                      children: [
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Image.asset(
-                          "assets/json.png",
-                          height: 18,
-                          width: 18,
-                        ),
-                        Text("  contact.json   ",
-                            style: TextStyle(color: Colors.white))
-                      ],
-                    )),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          PagesIndex = 2;
+                        });
+                      },
+                      child: Container(
+                          child: Row(
+                        children: [
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Image.asset(
+                            "assets/json.png",
+                            height: 18,
+                            width: 18,
+                          ),
+                          Text("  contact.json   ",
+                              style: TextStyle(color: Colors.white))
+                        ],
+                      )),
+                    ),
                     SizedBox(
                       width: 1,
                       child: Container(color: Colors.black),
